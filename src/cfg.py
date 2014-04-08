6c8e046cb9a469d190f7047a1360c8110fe151c6
penickamx@gmail.com <penickamx@gmail.com>
2014-04-08 22:50:39 +0200

    Multigraph support Cosmetic improvements

--- src/cfg.py
+++ src/cfg.py
@@ -22,31 +22,31 @@ Directed : 0/1
 ## Nodes
 # --------------------
 
-mode = 1
-num_nodes = 5
+mode                = 1
+num_nodes           = 5
 
 # --------------------
 ## Edges
 # --------------------
 
-edge_prob = 0.4
-cont = 0
-multi_g = 0
-multi_g_edge_prob = 0.1
-directed = 0
-loops = 0
+edge_prob           = 0.4
+cont                = 0
+multi_g             = 0             # 0 = off, x = max number of multi-edges
+multi_g_edge_prob   = 0.1
+directed            = 0
+loops               = 0
 
-edge_weights = 1
-edge_w_type = 0             # 0 = int, 1 = float
-edge_w_interval = [0, 10]
+edge_weights        = 1
+edge_w_type         = 0             # 0 = int, 1 = float
+edge_w_interval     = [0, 10]
 
 # --------------------
 ## Special settings
 # --------------------
 
-colored = 0
-tree = 0
-num_bridge = 0
-num_articulations = 0
-clique = 0
-bipartion = 0
+colored             = 0
+tree                = 0
+num_bridge          = 0
+num_articulations   = 0
+clique              = 0
+bipartion           = 0
--- src/generator.py
+++ src/generator.py
@@ -17,22 +17,37 @@ class Graphgen:
                 if node == node2 and cfg.loops == 0:
                     continue
                 
+                # add new edge
                 if r.random() < cfg.edge_prob:
                     
-                    weight = 1
-                    if cfg.edge_weights == 1:
-                        if cfg.edge_w_type == 0:
-                            weight = r.randint(cfg.edge_w_interval[0], cfg.edge_w_interval[1])
-                        else:
-                            pass
+                    weight = self._get_wight()
                     
                     graph.add_edge(node, node2, weight)
                     if cfg.directed == 0:
                         graph.add_edge(node2, node, weight)
+                    
+                    if cfg.multi_g > 0 and graph.mode != 1:
+                        for _ in range(cfg.multi_g):
+                            if r.random() < cfg.multi_g_edge_prob:
+                                weight = self._get_wight()
+                                
+                                graph.add_edge(node, node2, weight)
+                                if cfg.directed == 0:
+                                    graph.add_edge(node2, node, weight)                            
         
         return graph
     
+    def _get_wight(self):
+        weight = 1
+        if cfg.edge_weights == 1:
+                if cfg.edge_w_type == 0:
+                    weight = r.randint(cfg.edge_w_interval[0], cfg.edge_w_interval[1])
+                else:
+                    pass
+        return weight
     
+    def aa(self):
+        pass
     
 
 if __name__ == '__main__':
